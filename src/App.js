5f9454f7f6706e6a390f34cd8712bb83f5946f1f
Mary <you@example.com>
2023-11-17 20:57:16 +0400

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import './index.scss';
+import './index.scss'
+import { useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const onClickPlus = () => {
+    setCount(count + 1)
+  }
+
+  const onClickMinus = () => {
+    setCount(count - 1)
+  }
+
   return (
     <div className="App">
       <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h2>Counter:</h2>
+        <h1>{count}</h1>
+        <button onClick={onClickMinus} className="minus">
+          - Minus
+        </button>
+        <button onClick={onClickPlus} className="plus">
+          Plus +
+        </button>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
